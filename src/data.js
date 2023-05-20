4eb324069272173e9b0d180eae73ca2c8526db22
Ruobing <chaiyichai791@gmail.com>
2023-05-20 11:38:50 -0600

    adding firebase

---
+++ src/data.js
@@ -0,0 +1,7 @@
+const data =[
+    {id:1,body: "my first note"},
+    {id:2,body: "my second note"},
+    {id:3,body: "my third note"},
+    {id:4,body: "my forth note"}
+]
+export default data;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBoYzR26jJS8X0NuMO34QyahRLJI-t1fXE",
+  authDomain: "react-notebook-7ae99.firebaseapp.com",
+  projectId: "react-notebook-7ae99",
+  storageBucket: "react-notebook-7ae99.appspot.com",
+  messagingSenderId: "474536401886",
+  appId: "1:474536401886:web:24589fb292d339b1d65b4f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
